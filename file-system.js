058cbf84b53437c19d584a82772ef4ef6c8ad6b3
Nayan <dreamerboynayan@gmail.com>
2025-04-09 19:20:17 +0600

    file read asynchronously

--- file-system.js
+++ file-system.js
@@ -2,7 +2,16 @@ const fs = require("fs");
 
 // File Read Synchronously 
 
-const readFile = fs.readFileSync("file.txt", "utf8");
-console.log(readFile);
+// const readFile = fs.readFileSync("file.txt", "utf8");
+// console.log(readFile);
 
-console.log("File Read Synchronously.");
+// console.log("File Read Synchronously.");
+
+// File Read Asynchronously
+
+fs.readFile("file.txt" , "utf8" , (err, data) => {
+    if (err) throw err 
+    console.log(data);
+});
+
+console.log("File Read Asynchronously");
