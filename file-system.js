10fe7a58b7fcef05d583b00e524db9c4cefe52c4
Nayan <dreamerboynayan@gmail.com>
2025-04-09 19:31:14 +0600

    file write Asynchronously

--- file-system.js
+++ file-system.js
@@ -18,5 +18,14 @@ const fs = require("fs");
 
 // File Write Synchronously 
 
-fs.writeFileSync("file1.txt" ,"Hello I'm from file-system.js !");
-console.log("File Write Successfully.");
+// fs.writeFileSync("file1.txt" ,"Hello I'm from file-system.js !");
+// console.log("File Write Successfully.");
+
+// File Write Asynchronously
+
+fs.writeFile("file3.txt" , "New text file created using Asynchronously ." ,(err) => {
+    if (err) throw err
+    console.log("File write successfully.");
+});
+
+console.log("File Created Asynchronously.");
